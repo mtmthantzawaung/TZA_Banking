fc6f5d3d8a990686fc5b98aef2f8c8a6ffdaee43
mtmthantzawaung <thantzawaung@metateammyanmar.com>
2025-07-24 10:56:33 +0630

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <section className="home">
+     <div className="home-content">
+      <header className="home-header bg-amber-800">Welcome, Ardian</header>
+      <Button>da</Button>
+     </div>
+    </section>
+  );
+}
